80dedaa9974f0b98fe413d903b36265f7d785c49
Wiktoryn Żerebecki <wzerebecki@awesome-ind.com>
2023-10-27 18:02:39 +0200

    1

--- build-uc-rust.sh
+++ build-uc-rust.sh
@@ -11,3 +11,11 @@ chmod +x uc_rust/target/release/uc
 sudo cp uc_rust/target/release/uc /usr/bin/
 
 # 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
+
+
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
