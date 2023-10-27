ca87f14fef2327a70490aca0da6f34a21ab5a94c
Wiktoryn Żerebecki <wzerebecki@awesome-ind.com>
2023-10-27 17:49:38 +0200

    s

---
+++ build-uc-rust.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# download latest git
+git pull
+
+# call ccargo
+cargo build -r --manifest-path uc_rust/
+
+chmod +x uc_rust/target/release/uc 
+# test if we can put a copy to /usr/bin/ here
+sudo cp uc_rust/target/release/uc /usr/bin/
