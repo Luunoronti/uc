36e6a991d7ce4797b87b125163a7339ff4d3ca7b
Wiktoryn Żerebecki <wzerebecki@awesome-ind.com>
2023-11-03 11:06:07 +0100

    1

--- uc/src/main.rs
+++ uc/src/main.rs
@@ -13,8 +13,8 @@ static mut STATUS_LEN: usize = 0;
 static mut CUR_X: i32 = 0;
 static mut CUR_Y: i32 = 0;
 
-pub mod tools;
 pub mod cpuprogress;
+pub mod tools;
 
 fn main() {
     if env::args().count() == 1 {
@@ -35,7 +35,10 @@ fn main() {
     loop {
         let size = match socket.recv_from(&mut buffer) {
             Ok(s) => s.0,
-            Err(_) => exit(-1),
+            Err(_) => {
+                hide_progress_safe();
+                exit(-1)
+            }
         };
 
         let data = &buffer[..size];
@@ -47,7 +50,11 @@ fn process_resp(data: &[u8]) {
     let code = data[0];
     unsafe {
         match code {
-            0x01 => exit(0), // command completed
+            0x01 => {
+                hide_progress_safe();
+                exit(0)
+            } // command completed
+
             0xF0 => {
                 hide_progress(); // make sure we hide progress. it may show later, but we must hide it for now
                 print!(
@@ -55,7 +62,7 @@ fn process_resp(data: &[u8]) {
                     std::str::from_utf8(&data[1..(data.len() - 1)]).unwrap()
                 )
             } // text message
-            0x30 => {}       // ping, do nothing
+            0x30 => {}               // ping, do nothing
             0x11 => hide_progress(), // hide progress
             0x10 => show_progress(data),
             _ => println!("{}", code),
@@ -63,6 +70,11 @@ fn process_resp(data: &[u8]) {
     }
 }
 
+fn hide_progress_safe() {
+    unsafe {
+        hide_progress();
+    }
+}
 unsafe fn hide_progress() {
     if PROGRESS_BEGAN == false {
         return;
@@ -129,7 +141,7 @@ unsafe fn show_progress(data: &[u8]) {
     let (cx, cy) = cursor::get_pos().expect("Getting the cursor position failed");
 
     // todo: print progress
-    
+
     let mut prog_val = (_prog / 5) as usize;
     if prog_val > 0 && prog_val < 20 {
         prog_val += 1;
@@ -155,7 +167,6 @@ unsafe fn show_progress(data: &[u8]) {
 
     print!(" \x1b[0m{:3}%\x1b[0m", _prog);
 
-
     let ttl_width_msg = wnd_width - 29;
     // print title, advance to new line
     print!("\x1b[1m\x1b[38;2;180;180;0m");
@@ -175,7 +186,10 @@ unsafe fn show_progress(data: &[u8]) {
     if LAST_MSG_LEN <= msg.len() {
         print!(" {:.wnd_width_msg$}\x1b[0m", msg);
     } else {
-        print!(" {:.wnd_width_msg$}\x1b[0m", format!("{:<LAST_MSG_LEN$}", msg));
+        print!(
+            " {:.wnd_width_msg$}\x1b[0m",
+            format!("{:<LAST_MSG_LEN$}", msg)
+        );
     }
 
     LAST_MSG_LEN = msg.len(); // size is 25 + msg
@@ -183,12 +197,11 @@ unsafe fn show_progress(data: &[u8]) {
 
     // construct string of CPU cores
     let offset: usize = 6 + title_size + msg_size;
-      
+
     STATUS_LEN = cpuprogress::print_cpu_mem_progress(data, offset);
-   
+
     cursor::set_pos(cx, cy).expect("Set failed again");
 }
-
 
 fn print_help_and_exit() {
     println!(
